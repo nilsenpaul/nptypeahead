f01d22a8c1fd3641e2fe464f64cddf08435259bb
Paul Verheul <paul@nilsenpaul.nl>
2015-02-04 11:16:49 +0100

    Improved service queries

--- nptypeahead/services/NpTypeAhead_DataService.php
+++ nptypeahead/services/NpTypeAhead_DataService.php
@@ -27,13 +27,13 @@ class NpTypeAhead_DataService extends BaseApplicationComponent
         
         // Get all field content and iterate through to find the fields we need. There must be a better way, though.
         $fieldContent = craft()->db->createCommand()
-       		->selectDistinct('c.*')
-       		->from('content c')
+       		->selectDistinct(implode(', ', $fieldHandles))
+       		->from('content')
        		->queryAll();
         
-        foreach($fieldContent as $key => $fields) {
+        foreach($fieldContent as $fields) {
         	foreach($fields as $fieldKey => $fieldValue) {
-        		if(in_array($fieldKey, $fieldHandles) && $fieldValue) {
+        		if($fieldValue) {
         			$this->results[] = $fieldValue;
         		}
         	}
